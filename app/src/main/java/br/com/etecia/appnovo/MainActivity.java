68e753d9ddc193d1b0939a74c181c2232965f687
beatrizslima <bialima.ricarte@gmail.com>
2022-12-01 19:34:35 -0300

    Criando List View

--- app/src/main/java/br/com/etecia/appnovo/MainActivity.java
+++ app/src/main/java/br/com/etecia/appnovo/MainActivity.java
@@ -3,12 +3,16 @@ package br.com.etecia.appnovo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+ListView listaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaFilmes = findViewById(R.id.listaFilmes);
     }
 }
